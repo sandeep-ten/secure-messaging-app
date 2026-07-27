35ff2ee4bc6dd2ee839cdb34fecc6e84b13a6555
Sandeep-ten <sandeepkumarkuruba@gmail.com>
2026-07-27 15:58:40 +0530

    Add RSA key generation

---
+++ encryption.py
@@ -0,0 +1,48 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_keys():
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    public_key = private_key.public_key()
+
+    with open("private_key.pem", "wb") as f:
+        f.write(
+
+            private_key.private_bytes(
+
+                encoding=serialization.Encoding.PEM,
+
+                format=serialization.PrivateFormat.PKCS8,
+
+                encryption_algorithm=serialization.NoEncryption()
+
+            )
+
+        )
+
+    with open("public_key.pem", "wb") as f:
+
+        f.write(
+
+            public_key.public_bytes(
+
+                encoding=serialization.Encoding.PEM,
+
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+
+            )
+
+        )
+
+    print("RSA Key Pair Generated Successfully")
+
+
+if __name__ == "__main__":
+
+    generate_keys()
